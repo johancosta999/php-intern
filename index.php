dc479f2f28635ab3962e2876fd836376208e946b
Johan Costa <159167858+johancosta999@users.noreply.github.com>
2026-09-16 07:05:46 +0530

    index php implemented with php syntax

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>CodeRunner</h1>
-</body>
-</html>
+<?php
+
+$full_name = 'Johan';
+$age = 23;
+$price = 22.5;
+$isMale = true;
+$list = ['gojo', 'goku', 'vegeta'];
+
+echo $full_name;
+
+
+
+
+
+?>
